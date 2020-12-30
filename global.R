65d0552ce34112df63003e1b80855a9ddc202196
simon-anasta <simon.anastasiadis@gmail.com>
2020-12-30 17:30:57 +1300

    board dimensions moved to config file

--- global.R
+++ global.R
@@ -12,51 +12,38 @@ library(dplyr)
 
 ## Parameters ------------------------------------------------------------------
 
-# board dimensions
-STORAGE_SLOTS = 3
-DECK_SLOTS = 1
-BOARD_WIDTH = STORAGE_SLOTS + DECK_SLOTS
-BOARD_LHS = 0
-BOARD_RHS = BOARD_LHS + BOARD_WIDTH - 1
-AI_ROW = 1
-SCORE_ROW = 0
-PLAYER_ROW = -1
-BOARD_HEIGHT = 1 + (max(AI_ROW, PLAYER_ROW, SCORE_ROW) 
-                    - min(AI_ROW, PLAYER_ROW, SCORE_ROW))
-
 # display parameters
 UNIT_WIDTH = 1
 UNIT_HEIGHT = 1
 BORDER = 0.05
 
 # game settings
+NUM_CARDS_VALUE_1 = 10
+NUM_CARDS_VALUE_2 = 10
+NUM_CARDS_VALUE_3 = 10
 DECK_SIZE = 30
 
-## Playspace points ------------------------------------------------------------
+## Playing board ---------------------------------------------------------------
 
-PLAYSPACE_DF = data.frame(row_y = rep(1:BOARD_HEIGHT, times = BOARD_WIDTH),
-                          col_x = rep(1:BOARD_WIDTH, each = BOARD_HEIGHT))
-
-PLAYSPACE_DF = PLAYSPACE_DF %>%
+DF_BOARD = read.csv("board_components.csv") %>%
   mutate(
-    is_ai = row_y == BOARD_HEIGHT,
-    is_player = row_y == 1,
-    is_deck = (is_ai & col_x == BOARD_WIDTH) | (is_player & col_x == 1),
-    is_storage = (is_ai | is_player) & !is_deck,
-    
-    x_mid = (col_x - 1 + BOARD_LHS) * UNIT_WIDTH,
+    x_mid = col_x * UNIT_WIDTH,
     x_min = x_mid - 0.5 * UNIT_WIDTH,
     x_max = x_mid + 0.5 * UNIT_WIDTH,
     x_inner_min = x_min + BORDER,
     x_inner_max = x_max - BORDER,
     
-    y_mid = case_when(is_ai ~ AI_ROW, is_player ~ PLAYER_ROW, TRUE ~ SCORE_ROW) * UNIT_HEIGHT,
+    y_mid = row_y * UNIT_HEIGHT,
     y_min = y_mid - 0.5 * UNIT_WIDTH,
     y_max = y_mid + 0.5 * UNIT_WIDTH,
     y_inner_min = y_min + BORDER,
-    y_inner_max = y_max - BORDER,
+    y_inner_max = y_max - BORDER
   )
 
-
-
-         
+# board dimensions
+BOARD_WIDTH_MIN = min(DF_BOARD$col_x)
+BOARD_WIDTH_MAX = max(DF_BOARD$col_x)
+BOARD_WIDTH = BOARD_WIDTH_MAX - BOARD_WIDTH_MIN + 1
+BOARD_HEIGHT_MIN = min(DF_BOARD$row_y)
+BOARD_HEIGHT_MAX = max(DF_BOARD$row_y)
+BOARD_HEIGHT = BOARD_HEIGHT_MAX - BOARD_HEIGHT_MIN + 1
--- server.R
+++ server.R
@@ -8,16 +8,25 @@
 
 shinyServer(function(input, output, session) {
   
+  # initialise
+  game_state = reactiveValues()
+  
+  game_state$player_deck = c(rep("1", NUM_CARDS_VALUE_1),
+                             rep("2", NUM_CARDS_VALUE_2),
+                             rep("3", NUM_CARDS_VALUE_3)) %>%
+    sample(DECK_SIZE)
+  
+  # plot
   output$plot1 <- renderPlot({
     
     
     ggplot() + 
       scale_x_continuous(name="x") + 
       scale_y_continuous(name="y") +
-      geom_rect(data=PLAYSPACE_DF,
-                mapping=aes(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, fill=is_storage), color="black", alpha=0.5) +
-      geom_text(data=PLAYSPACE_DF,
-                aes(x=x_mid, y=y_mid, label=is_deck), size=4)
+      geom_rect(data=DF_BOARD,
+                mapping=aes(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, fill=is_ai), color="black", alpha=0.5) +
+      geom_text(data=DF_BOARD,
+                aes(x=x_mid, y=y_mid, label=is_storage), size=4)
     
   })
   
