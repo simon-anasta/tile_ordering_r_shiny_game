dcfc7eec037f9edc362e862aa84adf84ff3dc6af
simon-anasta <simon.anastasiadis@gmail.com>
2021-01-01 12:56:31 +1300

    getters and setters

--- global.R
+++ global.R
@@ -9,6 +9,7 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(assertthat)
 
 ## Parameters ------------------------------------------------------------------
 
@@ -25,7 +26,12 @@ DECK_SIZE = 30
 
 ## Playing board ---------------------------------------------------------------
 
-DF_BOARD = read.csv("board_components.csv") %>%
+DF_BOARD = read.csv("board_components.csv")
+assert_that(all(c("component_name","row_y","col_x") %in% colnames(DF_BOARD)))
+assert_that(all(c("is_ai","is_player","is_storage") %in% colnames(DF_BOARD)))
+assert_that(length(DF_BOARD$component_name) == length(unique(DF_BOARD$component_name)))
+
+DF_BOARD = DF_BOARD %>%
   mutate(
     x_mid = col_x * UNIT_WIDTH,
     x_min = x_mid - 0.5 * UNIT_WIDTH,
@@ -37,7 +43,9 @@ DF_BOARD = read.csv("board_components.csv") %>%
     y_min = y_mid - 0.5 * UNIT_WIDTH,
     y_max = y_mid + 0.5 * UNIT_WIDTH,
     y_inner_min = y_min + BORDER,
-    y_inner_max = y_max - BORDER
+    y_inner_max = y_max - BORDER,
+    
+    value = NA
   )
 
 # board dimensions
@@ -47,3 +55,62 @@ BOARD_WIDTH = BOARD_WIDTH_MAX - BOARD_WIDTH_MIN + 1
 BOARD_HEIGHT_MIN = min(DF_BOARD$row_y)
 BOARD_HEIGHT_MAX = max(DF_BOARD$row_y)
 BOARD_HEIGHT = BOARD_HEIGHT_MAX - BOARD_HEIGHT_MIN + 1
+
+## Handle non-singletons -------------------------------------------------------
+
+handle_non_singletons <- function(answer, msg = "not singleton"){
+  
+  if(length(answer) == 1){
+    return(answer)
+  } else if(length(answer) == 0) {
+    return(NA)
+  } else if(length(answer) >= 2){
+    warning(msg)
+    return(answer)
+  } else {
+    stop("unreachible case")
+  }
+  
+}
+
+## Return location clicked -----------------------------------------------------
+
+component_clicked = function(mouse_x, mouse_y){
+  assert_that(is.numeric(mouse_x))
+  assert_that(is.numeric(mouse_y))
+  
+  answer = DF_BOARD %>%
+    mutate(clicked = (
+      x_min <= mouse_x 
+      & mouse_x < x_max
+      & y_min <= mouse_y
+      & mouse_y < y_max)
+    ) %>%
+    filter(clicked) %>%
+    select(component_name) %>%
+    unlist(use.names = FALSE)
+  
+  return(handle_non_singletons(answer))
+}
+
+## Get and Set value of component ----------------------------------------------
+
+get_component_value <- function(DF, component){
+  
+  answer = DF %>%
+    filter(component_name == component) %>%
+    select(value) %>%
+    unlist(use.names = FALSE)
+  
+  return(handle_non_singletons(answer))
+}
+
+set_component_value <- function(DF, component, new_value){
+  
+  answer_df = DF %>%
+    mutate(value = ifelse(component_name == component, new_value, value))
+  
+  return(answer_df)
+}
+
+##
